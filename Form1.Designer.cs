3b1549e4fa5c5fbadaafef19deabe0e53668c6d5
Andreas1593 <81862353+Andreas1593@users.noreply.github.com>
2022-02-04 18:33:13 +0100

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace timetracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timeSessionLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textSessionLabel = new System.Windows.Forms.Label();
+            this.timeTotalLabel = new System.Windows.Forms.Label();
+            this.textTotalLabel = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolOpenButton = new System.Windows.Forms.ToolStripButton();
+            this.toolCreateButton = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.projectLabel = new System.Windows.Forms.Label();
+            this.textTodayLabel = new System.Windows.Forms.Label();
+            this.timeTodayLabel = new System.Windows.Forms.Label();
+            this.timerIdle = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.startButton.Enabled = false;
+            this.startButton.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.startButton.ForeColor = System.Drawing.Color.Gray;
+            this.startButton.Location = new System.Drawing.Point(72, 144);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 43);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.EnabledChanged += new System.EventHandler(this.startButton_enabledChanged);
+            this.startButton.Click += new System.EventHandler(this.startButton_click);
+            this.startButton.Paint += new System.Windows.Forms.PaintEventHandler(this.startButton_paint);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.DarkSalmon;
+            this.stopButton.Enabled = false;
+            this.stopButton.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.stopButton.ForeColor = System.Drawing.Color.Gray;
+            this.stopButton.Location = new System.Drawing.Point(216, 144);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(100, 43);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Pause";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.EnabledChanged += new System.EventHandler(this.stopButton_enabledChanged);
+            this.stopButton.Click += new System.EventHandler(this.stopButton_click);
+            this.stopButton.Paint += new System.Windows.Forms.PaintEventHandler(this.stopButton_paint);
+            // 
+            // timeSessionLabel
+            // 
+            this.timeSessionLabel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.timeSessionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeSessionLabel.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timeSessionLabel.Location = new System.Drawing.Point(216, 220);
+            this.timeSessionLabel.Name = "timeSessionLabel";
+            this.timeSessionLabel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 4);
+            this.timeSessionLabel.Size = new System.Drawing.Size(152, 48);
+            this.timeSessionLabel.TabIndex = 2;
+            this.timeSessionLabel.Text = "00:00";
+            this.timeSessionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textSessionLabel
+            // 
+            this.textSessionLabel.AutoSize = true;
+            this.textSessionLabel.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textSessionLabel.Location = new System.Drawing.Point(12, 224);
+            this.textSessionLabel.Name = "textSessionLabel";
+            this.textSessionLabel.Size = new System.Drawing.Size(125, 41);
+            this.textSessionLabel.TabIndex = 3;
+            this.textSessionLabel.Text = "Session:";
+            // 
+            // timeTotalLabel
+            // 
+            this.timeTotalLabel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.timeTotalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeTotalLabel.Font = new System.Drawing.Font("Segoe UI Semilight", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timeTotalLabel.Location = new System.Drawing.Point(216, 343);
+            this.timeTotalLabel.Name = "timeTotalLabel";
+            this.timeTotalLabel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
+            this.timeTotalLabel.Size = new System.Drawing.Size(152, 38);
+            this.timeTotalLabel.TabIndex = 4;
+            this.timeTotalLabel.Text = "00:00";
+            this.timeTotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textTotalLabel
+            // 
+            this.textTotalLabel.AutoSize = true;
+            this.textTotalLabel.Font = new System.Drawing.Font("Segoe UI Semilight", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textTotalLabel.Location = new System.Drawing.Point(16, 347);
+            this.textTotalLabel.Name = "textTotalLabel";
+            this.textTotalLabel.Size = new System.Drawing.Size(63, 30);
+            this.textTotalLabel.TabIndex = 5;
+            this.textTotalLabel.Text = "Total:";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolOpenButton,
+            this.toolCreateButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(380, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolOpenButton
+            // 
+            this.toolOpenButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolOpenButton.Image = ((System.Drawing.Image)(resources.GetObject("toolOpenButton.Image")));
+            this.toolOpenButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolOpenButton.Name = "toolOpenButton";
+            this.toolOpenButton.Size = new System.Drawing.Size(23, 22);
+            this.toolOpenButton.Text = "Open project";
+            this.toolOpenButton.Click += new System.EventHandler(this.toolOpenButton_Click);
+            // 
+            // toolCreateButton
+            // 
+            this.toolCreateButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolCreateButton.Image = ((System.Drawing.Image)(resources.GetObject("toolCreateButton.Image")));
+            this.toolCreateButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolCreateButton.Name = "toolCreateButton";
+            this.toolCreateButton.Size = new System.Drawing.Size(23, 22);
+            this.toolCreateButton.Text = "Create a project";
+            this.toolCreateButton.Click += new System.EventHandler(this.toolCreateButton_click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // projectLabel
+            // 
+            this.projectLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.projectLabel.Location = new System.Drawing.Point(12, 40);
+            this.projectLabel.Name = "projectLabel";
+            this.projectLabel.Size = new System.Drawing.Size(349, 85);
+            this.projectLabel.TabIndex = 8;
+            this.projectLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textTodayLabel
+            // 
+            this.textTodayLabel.AutoSize = true;
+            this.textTodayLabel.Font = new System.Drawing.Font("Segoe UI Semilight", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textTodayLabel.Location = new System.Drawing.Point(14, 288);
+            this.textTodayLabel.Name = "textTodayLabel";
+            this.textTodayLabel.Size = new System.Drawing.Size(86, 36);
+            this.textTodayLabel.TabIndex = 9;
+            this.textTodayLabel.Text = "Today:";
+            // 
+            // timeTodayLabel
+            // 
+            this.timeTodayLabel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.timeTodayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeTodayLabel.Font = new System.Drawing.Font("Segoe UI Semilight", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timeTodayLabel.Location = new System.Drawing.Point(216, 284);
+            this.timeTodayLabel.Name = "timeTodayLabel";
+            this.timeTodayLabel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
+            this.timeTodayLabel.Size = new System.Drawing.Size(152, 42);
+            this.timeTodayLabel.TabIndex = 10;
+            this.timeTodayLabel.Text = "00:00";
+            this.timeTodayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timerIdle
+            // 
+            this.timerIdle.Interval = 1000;
+            this.timerIdle.Tick += new System.EventHandler(this.timerIdle_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(380, 392);
+            this.Controls.Add(this.timeTodayLabel);
+            this.Controls.Add(this.textTodayLabel);
+            this.Controls.Add(this.projectLabel);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.textTotalLabel);
+            this.Controls.Add(this.timeTotalLabel);
+            this.Controls.Add(this.textSessionLabel);
+            this.Controls.Add(this.timeSessionLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Time Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button startButton;
+        private Button stopButton;
+        private Label timeSessionLabel;
+        private System.Windows.Forms.Timer timer1;
+        private Label textSessionLabel;
+        private Label timeTotalLabel;
+        private Label textTotalLabel;
+        private Label projectLabel;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolCreateButton;
+        private ToolStripButton toolOpenButton;
+        private OpenFileDialog openFileDialog1;
+        private Label textTodayLabel;
+        private Label timeTodayLabel;
+        private System.Windows.Forms.Timer timerIdle;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,340 @@
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+// Usage: Debug.WriteLine("Text")
+
+namespace timetracker
+{
+    public partial class Form1 : Form
+    {
+        string path;
+
+        int timeSession = 0;
+        int timeToday = 0;
+        int timeTotal = 0;
+
+        // Time passed since the last break
+        // Saved to file on every break
+        int timePassed = 0;
+
+        bool idle = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeSession++;
+            timeToday++;
+            timeTotal++;
+            timePassed++;
+
+            timeSessionLabel.Text = formatTime(timeSession);
+            timeTodayLabel.Text = formatTime(timeToday);
+            timeTotalLabel.Text = formatTime(timeTotal);
+        }
+
+        private void startButton_click(object sender, EventArgs e)
+        {
+            startTimer();
+        }
+
+        private void stopButton_click(object sender, EventArgs e)
+        {
+            stopTimer();
+        }
+
+        private void toolCreateButton_click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                stopTimer();
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Title = "Create a new project";
+            sfd.Filter = "CSV file (*.csv)|*.csv";
+            sfd.FilterIndex = 2;
+            sfd.RestoreDirectory = true;
+            sfd.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "projects");
+            sfd.OverwritePrompt = false;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                path = sfd.FileName;
+
+                // Write project name into file
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(Path.GetFileNameWithoutExtension(path));
+                }
+
+                // Load file to update the GUI
+                loadProject(File.Open(path, FileMode.Open));
+            }
+        }
+
+        private void toolOpenButton_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                stopTimer();
+            }
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Title = "Open a project";
+            ofd.Filter = "CSV file (*.csv)|*.csv";
+            ofd.RestoreDirectory = true;
+            Debug.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            ofd.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "projects");
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.FileName;
+                loadProject(ofd.OpenFile());
+            }
+        }
+
+        private void startButton_enabledChanged(object sender, EventArgs e)
+        {
+            if (startButton.Enabled)
+            {
+                startButton.BackColor = Color.LimeGreen;
+                startButton.ForeColor = Color.Black;
+            }
+            else
+            {
+                timer1.Start();
+                timerIdle.Start();
+
+                startButton.BackColor = Color.DarkSeaGreen;
+                startButton.ForeColor = Color.Gray;
+            }
+        }
+
+        private void stopButton_enabledChanged(object sender, EventArgs e)
+        {
+            if (stopButton.Enabled)
+            {
+                stopButton.BackColor = Color.Tomato;
+                stopButton.ForeColor = Color.Black;
+            }
+            else
+            {
+                stopButton.BackColor = Color.Salmon;
+                stopButton.ForeColor = Color.Gray;
+
+                // Save the time when the user stops or opens a new dialog
+                timer1.Stop();
+
+                if (timePassed != 0)
+                {
+                    string content = DateTime.Now.ToString("yyyy-MM-dd") + ";" +
+                                     DateTime.Now.ToString("HH:mm") + ";" + timePassed;
+                    updateFile(content);
+                }
+
+                timePassed = 0;
+            }
+        }
+
+        private void startButton_paint(object sender, PaintEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            // Make sure Text is not also written on button
+            btn.Text = string.Empty;
+
+            // Set flags to center text on button
+            TextFormatFlags flags = TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter | TextFormatFlags.WordBreak;
+
+            // Render the text onto the button
+            TextRenderer.DrawText(e.Graphics, "Start", btn.Font, e.ClipRectangle, btn.ForeColor, flags);
+        }
+
+        private void stopButton_paint(object sender, PaintEventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Text = string.Empty;
+            TextFormatFlags flags = TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter | TextFormatFlags.WordBreak;
+            TextRenderer.DrawText(e.Graphics, "Stop", btn.Font, e.ClipRectangle, btn.ForeColor, flags);
+        }
+
+        private void loadProject(Stream fileStream)
+        {
+            using (var streamReader = new StreamReader(fileStream))
+            {
+                string line;
+                string today = DateTime.Now.ToString("yyyy-MM-dd").ToString();
+                timeSession = 0;
+                timeToday = 0;
+                timeTotal = 0;
+
+                // Display project name
+                if ((line = streamReader.ReadLine()) != null)
+                {
+                    projectLabel.Text = line;
+                }
+
+                // Sum up total time
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    // Prevent IndexOutOfRangeException
+                    if (line.Length > 17)
+                    {
+                        int session = Int32.Parse(line[17..]);
+
+                        // Sum up total time
+                        timeTotal += session;
+
+                        // Sum up today time
+                        if (line[..10] == today)
+                        {
+                            timeToday += session;
+                        }
+                    }
+                }
+
+                timeSessionLabel.Text = formatTime(timeSession);
+                timeTodayLabel.Text = formatTime(timeToday);
+                timeTotalLabel.Text = formatTime(timeTotal);
+
+                startButton.Enabled = true;
+            }
+        }
+
+        private void updateFile(string content)
+        {
+            // Write the latest record into the file
+            string projectName = projectLabel.Text;
+            try
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(content);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access to the path '" + path + "' denied (no record saved).");
+            }
+        }
+
+        private void startTimer()
+        {
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+        }
+
+        private void stopTimer()
+        {
+            stopButton.Enabled = false;
+            startButton.Enabled = true;
+        }
+
+        private string formatTime(int time)
+        {
+            // Time is saved in seconds
+
+            int hr = time / 3600;
+            int remainingSec = time % 3600;
+            int min = remainingSec / 60;
+            remainingSec = remainingSec % 60;
+            int sec = remainingSec % 60;
+
+            string hrStr;
+            if (hr < 10)
+            {
+                hrStr = "0" + hr.ToString();
+            }
+            else
+            {
+                hrStr = hr.ToString();
+            }
+
+            string minStr;
+            if (min < 10)
+            {
+                minStr = "0" + min.ToString();
+            }
+            else
+            {
+                minStr = min.ToString();
+            }
+
+            string secStr;
+            if (sec < 10)
+            {
+                secStr = "0" + sec.ToString();
+            }
+            else
+            {
+                secStr = sec.ToString();
+            }
+
+            return hrStr + ":" + minStr + ":" + secStr;
+        }
+
+        // Unmanaged function from user32.dll    
+        [DllImport("user32.dll")]
+        static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        // Struct we'll need to pass to the function    
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+
+        private void timerIdle_Tick(object sender, EventArgs e)
+        {
+            // Get the system uptime    
+            int systemUptime = Environment.TickCount;
+            // The tick at which the last input was recorded    
+            int LastInputTicks = 0;
+            // The number of ticks that passed since last input    
+            int IdleTicks = 0;
+            // Set the struct    
+            LASTINPUTINFO LastInputInfo = new LASTINPUTINFO();
+            LastInputInfo.cbSize = (uint)Marshal.SizeOf(LastInputInfo);
+            LastInputInfo.dwTime = 0;
+
+            // If we have a value from the function    
+            if (GetLastInputInfo(ref LastInputInfo))
+            {
+                // Get the number of ticks at the point when the last activity was seen    
+                LastInputTicks = (int)LastInputInfo.dwTime;
+                // Number of idle ticks = system uptime ticks - number of ticks at last input    
+                IdleTicks = systemUptime - LastInputTicks;
+                Debug.WriteLine(IdleTicks);
+
+                // Pause timer without changing button states
+                if (idle && IdleTicks < 500)
+                {
+                    timer1.Start();
+                    idle = false;
+                }
+                // Idle after 1 minute without input
+                else if (IdleTicks > 60000)
+                {
+                    timer1.Stop();
+                    idle = true;
+                }
+            }
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
